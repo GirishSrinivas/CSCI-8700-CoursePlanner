92d93669ac5657ecd424c7cf5fca310bb631627c
GirishSrinivas <girish.s.srinivas@gmail.com>
2016-11-11 08:37:57 -0600

    modified LoginDAO interface by adding login functionality

--- CoursePlanner/src/com/girish/DAOLayers/LoginDAO.java
+++ CoursePlanner/src/com/girish/DAOLayers/LoginDAO.java
@@ -1,11 +1,13 @@
 package com.girish.DAOLayers;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface LoginDAO 
 {
-	public List<LoginBean> read();
-	public void write(LoginBean bean);
-	public void update(LoginBean bean);
-	public void delete(LoginBean bean);
+	public List<LoginBean> read() throws ClassNotFoundException, SQLException;
+	public void write(LoginBean bean) throws ClassNotFoundException, SQLException;
+	public void update(LoginBean bean) throws ClassNotFoundException, SQLException;
+	public void delete(LoginBean bean) throws ClassNotFoundException, SQLException;
+	public boolean login(LoginBean bean) throws ClassNotFoundException, SQLException;
 }

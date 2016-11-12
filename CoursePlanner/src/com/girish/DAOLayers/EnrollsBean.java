0085de694b0ed52b5fa64806b5876abd6cc4fb52
GirishSrinivas <girish.s.srinivas@gmail.com>
2016-11-12 01:58:14 -0600

    added EnrollsBean

---
+++ CoursePlanner/src/com/girish/DAOLayers/EnrollsBean.java
@@ -0,0 +1,48 @@
+package com.girish.DAOLayers;
+
+public class EnrollsBean 
+{
+	private int nuid;
+	private String sec_id;
+	private String term;
+	private int year;
+	
+	public EnrollsBean()
+	{
+		
+	}
+
+	public int getNuid() {
+		return nuid;
+	}
+
+	public void setNuid(int nuid) {
+		this.nuid = nuid;
+	}
+
+	public String getSec_id() {
+		return sec_id;
+	}
+
+	public void setSec_id(String sec_id) {
+		this.sec_id = sec_id;
+	}
+
+	public String getTerm() {
+		return term;
+	}
+
+	public void setTerm(String term) {
+		this.term = term;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+}	

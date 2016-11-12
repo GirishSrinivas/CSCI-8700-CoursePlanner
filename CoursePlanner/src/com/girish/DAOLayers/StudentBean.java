e8eb1a6197e96908a0de7f4ecc48fc8246d3014c
GirishSrinivas <girish.s.srinivas@gmail.com>
2016-11-12 02:07:57 -0600

    added Student bean file

---
+++ CoursePlanner/src/com/girish/DAOLayers/StudentBean.java
@@ -0,0 +1,75 @@
+package com.girish.DAOLayers;
+
+public class StudentBean  
+{
+	private int nuid;
+	private String net_id;
+	private String fname;
+	private String lname;
+	private String level;
+	private String major;
+	private String concentration;
+	
+	public StudentBean()
+	{
+		
+	}
+
+	public int getNuid() {
+		return nuid;
+	}
+
+	public void setNuid(int nuid) {
+		this.nuid = nuid;
+	}
+
+	public String getNet_id() {
+		return net_id;
+	}
+
+	public void setNet_id(String net_id) {
+		this.net_id = net_id;
+	}
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+	public String getLname() {
+		return lname;
+	}
+
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+
+	public String getLevel() {
+		return level;
+	}
+
+	public void setLevel(String level) {
+		this.level = level;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public String getConcentration() {
+		return concentration;
+	}
+
+	public void setConcentration(String concentration) {
+		this.concentration = concentration;
+	}
+	
+	
+}

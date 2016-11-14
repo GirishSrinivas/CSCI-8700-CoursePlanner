edfc7c31bb746fc8acca4c643fe099ea08e3227a
GirishSrinivas <girish.s.srinivas@gmail.com>
2016-11-13 23:16:07 -0600

    changed database design

--- Database_Design/course_planner.sql
+++ Database_Design/course_planner.sql
@@ -10,4 +10,15 @@ create table login(net_id varchar(25),
     -> pwd varchar(25),
     -> primary key (net_id));
 
+/* for student table */
+
+create table student(nuid int,
+    -> net_id varchar(25),
+    -> fname varchar(25),
+    -> lname varchar(25),
+    -> level varchar(25),
+    -> major varchar(25),
+    -> concentration varchar(25),
+    -> primary key(nuid));  
+
 
